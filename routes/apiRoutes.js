751aeb81ce5665df712b4805514c3efc4d2a5e69
Diane Reeves <dianehreeves@gmail.com>
2020-07-30 22:07:46 -0500

    removed node modules

---
+++ routes/apiRoutes.js
@@ -0,0 +1,54 @@
+//jshint esversion:6
+
+const Workout = require("../models /workout");
+const { workout } = require("../models");
+
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+        workout.find()
+            .then(data => {
+                res.json(data);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts", function (req, res) {
+        workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", function (req, res) {
+        workout.find()
+            .then(data => {
+                res.json(data);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts/range", function (req, res) {
+        workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.put("/api/worouts/:id", ({ body, params }, res) => {
+        workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                res.json(err);
+            });
+    });
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+
+//jshint esversion:6
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log('App listening on Port ${PORT}!');
+});
+
+
+
